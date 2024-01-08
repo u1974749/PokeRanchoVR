8b7eb4707983225f915cc39459bd6f70ad8ae6f3
u1973234 <99675616+u1973234@users.noreply.github.com>
2024-01-08 14:47:11 +0100

    detectado grip

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.XR;
 
@@ -7,11 +9,37 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] GameObject pokeball;
     [SerializeField] Pokeball pokeballScript;
-    Vector3 RighHandPosition = new();
-    void Start(){
+    [SerializeField] GameObject pokeballProjectile;
+    
+    private Vector3 RighHandPosition = new();
+    List<UnityEngine.XR.InputDevice> rightHandedControllers = new List<UnityEngine.XR.InputDevice>();
+    private bool gripL;
+    
+    private void Start(){
+        
         createPokeball();
+
+
         //addAllPokeballs();
     }
+
+    private void Update(){
+
+        if (rightHandedControllers.Count <= 0)
+        {
+            var rdesiredCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller;
+            UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(rdesiredCharacteristics, rightHandedControllers);
+        }
+
+        bool triggerValue;
+        foreach (UnityEngine.XR.InputDevice device in rightHandedControllers)
+        {
+            if(device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+                //Instantiate()
+                Debug.Log("pulsando");
+        }
+    }
+
     public void createPokeball(){
         RighHandPosition = GameObject.FindGameObjectWithTag("RightHand").transform.position;
         Debug.Log("PositionHand"+RighHandPosition);
