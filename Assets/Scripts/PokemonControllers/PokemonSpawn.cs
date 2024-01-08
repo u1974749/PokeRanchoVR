f67d1f3bd8f3270a0f84450208941884222c6ce3
u1974750 <99675264+u1974750@users.noreply.github.com>
2024-01-08 13:48:14 +0100

    pokemon spawn

---
+++ Assets/Scripts/PokemonControllers/PokemonSpawn.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonSpawn : MonoBehaviour
+{
+    [SerializeField] ListOfPokemon listOfPokemon;
+    public GameObject[] spawnPoints;
+    private GameObject[] allPkmn;
+
+    private int Spawned = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allPkmn = listOfPokemon.allPokemon;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnPokemon();
+    }
+
+    private void SpawnPokemon()
+    {
+        GameObject[] pokemonInScene = null;
+        pokemonInScene = GameObject.FindGameObjectsWithTag("pokemon");
+        if (pokemonInScene.Length < 7)
+        {
+            SpawnRandomPokemon();
+        }
+    }
+
+    private void SpawnRandomPokemon()
+    {
+        int index = Random.Range(0, allPkmn.Length);
+        int pos = Random.Range(0, 4);
+
+        Instantiate(allPkmn[index], spawnPoints[pos].transform.position, Quaternion.identity);
+        
+    }
+
+}
